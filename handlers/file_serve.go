034b0183669843be28a65442adc51031c33df061
Riya Dennis <riyacv@gmail.com>
2018-06-06 20:31:24 +0100

    Added structs to marshal

--- handlers/file_serve.go
+++ handlers/file_serve.go
@@ -5,27 +5,43 @@ import (
 	"github.com/julienschmidt/httprouter"
 	"github.com/sirupsen/logrus"
 	"github.com/riyadennis/redis-wrapper"
-	"fmt"
+	"encoding/json"
+	"regexp"
+	"strings"
 )
 
+type Files struct {
+	Files []FileResponse `json: "files"`
+}
+type FileResponse struct {
+	Filename string `json: "filename"`
+}
+
 func FileServe(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	var keys []string
+	var cursor uint64
 	c := redis_wrapper.Client{}
+	res := make([]FileResponse, 1)
 	client, err := c.Create()
 	if err != nil {
 		logrus.Error(err.Error())
 	}
-	var keys []string
-	var cursor uint64
 	keys, cursor, err = client.RedisClient.Scan(cursor, "", 100).Result()
-	detail := "{"
 	for _, k := range keys {
 		val, err := client.Get(k)
 		if err != nil {
 			logrus.Error(err.Error())
 		}
-		detail += fmt.Sprintf("%s \n", val)
+		fr := FileResponse{val}
+		res = append(res, fr)
 	}
-	detail += "}"
+	files := Files{Files: res}
+	fileJson, err := json.MarshalIndent(files, " ", " ")
+	detail := cleanResponse(string(fileJson))
 	api := createResponse(detail, "success", http.StatusOK)
 	jsonResponseDecorator(api, w)
+}
+func cleanResponse(jsonString string) string {
+	reg := regexp.MustCompile("/(\r\n)+|\r+|\n+|\t+/i")
+	return strings.Replace(reg.ReplaceAllString(jsonString, " "), "\\", " ", -1)
 }
