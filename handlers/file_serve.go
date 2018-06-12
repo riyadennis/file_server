35e2e055df4292d334585a530408e7680d7d5c25
Riya Dennis <riyacv@gmail.com>
2018-06-12 20:41:44 +0100

    Removed limitation of 100 keys

--- handlers/file_serve.go
+++ handlers/file_serve.go
@@ -6,42 +6,36 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/riyadennis/redis-wrapper"
 	"encoding/json"
-	"regexp"
-	"strings"
 )
 
 type Files struct {
 	Files []FileResponse `json: "files"`
 }
 type FileResponse struct {
-	Filename string `json: "filename"`
+	Filename string `json: "file_name"`
 }
 
 func FileServe(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
-	var keys []string
-	var cursor uint64
 	c := redis_wrapper.Client{}
 	res := make([]FileResponse, 1)
 	client, err := c.Create()
 	if err != nil {
 		logrus.Error(err.Error())
 	}
-	keys, cursor, err = client.RedisClient.Scan(cursor, "", 100).Result()
-	for _, k := range keys {
+	keys := client.RedisClient.Keys("*")
+	for _, k := range keys.Val() {
 		val, err := client.Get(k)
 		if err != nil {
 			logrus.Error(err.Error())
 		}
-		fr := FileResponse{val}
-		res = append(res, fr)
+		if val != ""{
+			fr := FileResponse{val}
+			res = append(res, fr)
+		}
 	}
 	files := Files{Files: res}
-	fileJson, err := json.MarshalIndent(files, " ", " ")
-	detail := cleanResponse(string(fileJson))
+	fileJson, err := json.Marshal(files)
+	detail := string(fileJson)
 	api := createResponse(detail, "success", http.StatusOK)
 	jsonResponseDecorator(api, w)
-}
-func cleanResponse(jsonString string) string {
-	reg := regexp.MustCompile("/(\r\n)+|\r+|\n+|\t+/i")
-	return strings.Replace(reg.ReplaceAllString(jsonString, " "), "\\", " ", -1)
 }
